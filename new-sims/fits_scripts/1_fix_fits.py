821b643b8c623b8513b4cf972549b65275ccc480
Keith Buckholz <keith.buckholz@yale.edu>
2024-06-18 12:55:00 -0400

    cleaned fits folder; changed fits filenames; made fits prep pipline clearer

--- new-sims/fits_scripts/1_fix_fits.py
+++ new-sims/fits_scripts/1_fix_fits.py
@@ -7,5 +7,6 @@ direct_file = file
 direct_fits = fits.open(direct_file)
 direct_fits[0].header["INSTRUME"] = "ROMAN"
 direct_fits[0].header["FILTER"] = "det1"
+direct_fits[1].header["CONFFILE"] = "/Users/keith/astr/research_astr/FOV0/Roman.det1.07242020.conf"
 
 direct_fits.writeto(newfile, overwrite=True)
