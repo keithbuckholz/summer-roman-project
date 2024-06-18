9b8586ecb6bdadf5c96ff179a020a92b26bd5e5f
Keith Buckholz <keith.buckholz@yale.edu>
2024-06-18 09:37:31 -0400

    Seperated new-sims contents into directories

---
+++ new-sims/fits_scripts/fix_fits.py
@@ -0,0 +1,11 @@
+from astropy.io import fits
+import sys
+
+_, file, newfile = sys.argv
+
+direct_file = file
+direct_fits = fits.open(direct_file)
+direct_fits[0].header["INSTRUME"] = "ROMAN"
+direct_fits[0].header["FILTER"] = "det1"
+
+direct_fits.writeto(newfile, overwrite=True)
